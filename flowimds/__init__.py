f1dcf4ef8b4c3b1f9b05fa491a52cfcb9527f358
mori-318 <105962185+mori-318@users.noreply.github.com>
2026-01-08 17:18:03 +0900

    feat: use explicit exports in __init__.py for better IDE support

--- flowimds/__init__.py
+++ flowimds/__init__.py
@@ -1,19 +1,22 @@
 """Convenient public API exports for ``flowimds``."""
 
-from flowimds.models import OutputMapping, ProcessedImage
-from flowimds.pipeline import Pipeline
-from flowimds.result import PipelineResult
+from flowimds.models import OutputMapping as OutputMapping, ProcessedImage as ProcessedImage
+from flowimds.pipeline import Pipeline as Pipeline
+from flowimds.result import PipelineResult as PipelineResult
 from flowimds.steps import (
-    BinarizeStep,
-    DenoiseStep,
-    FlipStep,
-    GrayscaleStep,
-    PipelineStep,
-    ResizeStep,
-    RotateStep,
+    BinarizeStep as BinarizeStep,
+    DenoiseStep as DenoiseStep,
+    FlipStep as FlipStep,
+    GrayscaleStep as GrayscaleStep,
+    PipelineStep as PipelineStep,
+    ResizeStep as ResizeStep,
+    RotateStep as RotateStep,
 )
-from flowimds.utils.image_discovery import IMAGE_SUFFIXES, collect_image_paths
-from flowimds.utils.image_io import read_image, write_image
+from flowimds.utils.image_discovery import (
+    IMAGE_SUFFIXES as IMAGE_SUFFIXES,
+    collect_image_paths as collect_image_paths,
+)
+from flowimds.utils.image_io import read_image as read_image, write_image as write_image
 
 __all__ = [
     "Pipeline",
